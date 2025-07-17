04820ec03da93993082f4c482bc618e90b308bf6
Dee-Hub25 <douglasmuri9@gmail.com>
2025-07-17 22:03:57 +0300

    Initial commit: django-models with relationship_app and auth templates

--- LibraryProject/relationship_app/views.py
+++ LibraryProject/relationship_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in the user after successful registration
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
